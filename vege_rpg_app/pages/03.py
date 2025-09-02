95c9c283268069095a229d967f784a0e20365f63
Bunbu- <kanrapapurukku@icloud.com>
2025-09-02 19:04:50 +0900

    変更

--- vege_rpg_app/pages/03.py
+++ vege_rpg_app/pages/03.py
@@ -8,10 +8,6 @@ st.write(f"画像パス: {image_path}")
 st.write(f"存在する？: {os.path.exists(image_path)}")
 
 if os.path.exists(image_path):
-    try:
-        img = Image.open(image_path)
-        st.image(img, caption="未開放称号", width=150)
-    except Exception as e:
-        st.error(f"画像の読み込みに失敗しました：{e}")
+    st.image("assets/images/titles/savior.png", width=150)
 else:
     st.warning("画像ファイルが見つかりません")
